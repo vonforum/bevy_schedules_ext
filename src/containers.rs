c01b2b248948f511f6afc6265328addae8223f90
vonforum <vonforum@users.noreply.github.com>
2024-03-21 23:17:09 +0200

    Add Container trait, make nesting use it, remove redundant resources

--- src/containers.rs
+++ src/containers.rs
@@ -1,16 +1,27 @@
 //! Extends Bevy's World with helpers for managing containers for schedules.
 
 use bevy_ecs::{prelude::*, schedule::InternedScheduleLabel};
-use bevy_utils::{HashMap, HashSet};
+use bevy_utils::HashMap;
+
+/// Exports the container world extension, plus the [`Container`] trait.
+pub mod prelude {
+	pub use super::{Container, ScheduleContainers, WorldExt as ContainersWorldExt};
+}
+
+/// A container of schedules that can be run.
+pub trait Container: Send + Sync + 'static {
+	/// Runs the container of schedules.
+	fn run(&self, world: &mut World);
+}
 
 /// A resource containing a map of a parent schedule to a container of child schedule(s).
 #[derive(Resource)]
-pub struct ScheduleContainers<S> {
+pub struct ScheduleContainers<S: Container> {
 	/// The map of parent schedules to containers.
 	pub inner: HashMap<InternedScheduleLabel, S>,
 }
 
-impl<S> Default for ScheduleContainers<S> {
+impl<S: Container> Default for ScheduleContainers<S> {
 	fn default() -> Self {
 		Self {
 			inner: HashMap::default(),
@@ -18,56 +29,57 @@ impl<S> Default for ScheduleContainers<S> {
 	}
 }
 
-/// A marker for whether a system to run the container of a schedule has been added.
-#[derive(Resource)]
-pub struct ScheduleSystems<S> {
-	/// The set of schedules that have a system to run their container.
-	pub inner: HashSet<InternedScheduleLabel>,
-	phantom: std::marker::PhantomData<S>,
-}
+/// Helper to create a system that runs a container.
+pub fn run_container_system<S: Container>(label: InternedScheduleLabel) -> impl FnMut(&mut World) {
+	move |world: &mut World| {
+		// Remove the children from the container. This is so we don't break ownership rules.
+		let children = {
+			let mut container = world.resource_mut::<ScheduleContainers<S>>();
+			container.inner.remove(&label).unwrap()
+		};
 
-impl<S> Default for ScheduleSystems<S> {
-	fn default() -> Self {
-		Self {
-			inner: HashSet::default(),
-			phantom: std::marker::PhantomData,
-		}
+		// Run the children
+		children.run(world);
+
+		// Add the children back to the container
+		// Unchecked, because we just removed it before
+		world
+			.resource_mut::<ScheduleContainers<S>>()
+			.inner
+			.insert_unique_unchecked(label, children);
 	}
 }
 
 /// Adds methods to [`World`] for managing schedule containers.
 pub trait WorldExt {
 	/// Initializes the [`ScheduleContainers`] resource and inserts a new default container for the given label.
-	fn init_schedule_container<S: FromWorld + Send + Sync + 'static>(
+	///
+	/// Returns `true` if the container was inserted, `false` if it was already present.
+	fn init_schedule_container<S: FromWorld + Container>(
 		&mut self,
 		label: InternedScheduleLabel,
-	);
+	) -> bool;
 
 	/// Initializes the [`ScheduleContainers`] resource and inserts a container for the given label, if not yet present.
-	fn insert_schedule_container<S: Send + Sync + 'static>(
+	///
+	/// Returns `true` if the container was inserted, `false` if it was already present.
+	fn insert_schedule_container<S: Container>(
 		&mut self,
 		label: InternedScheduleLabel,
 		container: S,
 	) -> bool;
-
-	/// Helper to mark whether a system to run the container of a schedule has been added.
-	/// Returns `true` if the marker was inserted, `false` if it was already present.
-	fn insert_schedule_container_system_marker<S: Send + Sync + 'static>(
-		&mut self,
-		label: InternedScheduleLabel,
-	) -> bool;
 }
 
 impl WorldExt for World {
-	fn init_schedule_container<S: FromWorld + Send + Sync + 'static>(
+	fn init_schedule_container<S: FromWorld + Container>(
 		&mut self,
 		label: InternedScheduleLabel,
-	) {
+	) -> bool {
 		let container = S::from_world(self);
-		self.insert_schedule_container(label, container);
+		self.insert_schedule_container(label, container)
 	}
 
-	fn insert_schedule_container<S: Send + Sync + 'static>(
+	fn insert_schedule_container<S: Container>(
 		&mut self,
 		label: InternedScheduleLabel,
 		container: S,
@@ -80,14 +92,5 @@ impl WorldExt for World {
 		} else {
 			false
 		}
-	}
-
-	fn insert_schedule_container_system_marker<S: Send + Sync + 'static>(
-		&mut self,
-		label: InternedScheduleLabel,
-	) -> bool {
-		let mut systems = self.get_resource_or_insert_with(ScheduleSystems::<S>::default);
-
-		systems.inner.insert(label)
 	}
 }
--- src/nesting.rs
+++ src/nesting.rs
@@ -6,27 +6,45 @@ use bevy_ecs::{
 };
 use bevy_utils::all_tuples;
 
+use crate::containers::prelude::*;
+
 /// Exports the app extension if the feature is enabled.
 pub mod prelude {
 	#[cfg(feature = "app_ext")]
 	pub use super::app_ext::AppExt as NestingAppExt;
 }
 
+/// Container for nested schedules.
+#[derive(Default)]
+pub struct NestedSchedules {
+	/// Vector of schedules.
+	pub inner: Vec<InternedScheduleLabel>,
+}
+
+impl Container for NestedSchedules {
+	fn run(&self, world: &mut World) {
+		self.inner.iter().for_each(|&label| {
+			world.run_schedule(label);
+		});
+	}
+}
 /// A trait for converting a schedule or a tuple of schedules into a container compatible form.
 pub trait SchedulesIntoConfigs<Marker>
 where
 	Self: Sized,
 {
 	/// Converts the schedule or tuple of schedules into a vector of labels.
-	fn to_vec(&self) -> Vec<InternedScheduleLabel>;
+	fn to_container(&self) -> NestedSchedules;
 }
 
 impl<T> SchedulesIntoConfigs<()> for T
 where
 	T: ScheduleLabel,
 {
-	fn to_vec(&self) -> Vec<InternedScheduleLabel> {
-		vec![self.intern()]
+	fn to_container(&self) -> NestedSchedules {
+		NestedSchedules {
+			inner: vec![self.intern()],
+		}
 	}
 }
 
@@ -40,9 +58,11 @@ macro_rules! impl_schedules_into_configs {
             $($sys: ScheduleLabel),*
         {
             #[allow(non_snake_case)]
-			fn to_vec(&self) -> Vec<InternedScheduleLabel> {
+			fn to_container(&self) -> NestedSchedules {
 				let ($($name,)*) = self;
-				vec![$($name.intern(),)*]
+				NestedSchedules {
+					inner: vec![$($name.intern(),)*]
+				}
 			}
         }
     }
@@ -50,36 +70,13 @@ macro_rules! impl_schedules_into_configs {
 
 all_tuples!(impl_schedules_into_configs, 1, 20, S, s, l);
 
-/// Helper to create a system that runs the children of a schedule.
-pub fn create_run_children_system(label: InternedScheduleLabel) -> impl FnMut(&mut World) {
-	type Containers = crate::containers::ScheduleContainers<Vec<InternedScheduleLabel>>;
-	move |world: &mut World| {
-		// Remove the children from the container. This is so we don't break ownership rules.
-		let children = {
-			let mut container = world.resource_mut::<Containers>();
-			container.inner.remove(&label).unwrap()
-		};
-
-		// Run the children
-		children.iter().for_each(|&label| {
-			world.run_schedule(label);
-		});
-
-		// Add the children back to the container
-		// Unchecked, because we just removed it before
-		world
-			.resource_mut::<Containers>()
-			.inner
-			.insert_unique_unchecked(label, children);
-	}
-}
-
 /// Adds methods to the [`App`] type for working with nested schedules.
 #[cfg(feature = "app_ext")]
 pub mod app_ext {
 	use bevy_app::prelude::*;
 
 	use super::*;
+	use crate::containers::run_container_system;
 
 	/// Adds the [`add_schedules`](App::add_schedules) method to the `App` type.
 	pub trait AppExt {
@@ -120,33 +117,28 @@ pub mod app_ext {
 			parent: P,
 			children: S,
 		) -> &mut Self {
+			let container = children.to_container();
 			// Initialize child schedules in the world
-			children.to_vec().iter().for_each(|&label| {
+			container.inner.iter().for_each(|&label| {
 				self.init_schedule(label);
 			});
 
 			let label = parent.intern();
 
-			use crate::containers::*;
-
-			type Inner = Vec<InternedScheduleLabel>;
-
-			self.world.init_schedule_container::<Inner>(label); // Initialize the container if not yet present
-			if self
-				.world
-				.insert_schedule_container_system_marker::<Inner>(label)
-			{
-				// If the system to run the child schedules isn't present yet, add it
-				self.add_systems(label, create_run_children_system(label));
+			// Initialize the container if not yet present
+			if self.world.init_schedule_container::<NestedSchedules>(label) {
+				// If the container wasn't present, add a system to run the children
+				self.add_systems(label, run_container_system::<NestedSchedules>(label));
 			}
 
 			// Add the children to the container
 			self.world
-				.resource_mut::<ScheduleContainers<Inner>>()
+				.resource_mut::<ScheduleContainers<NestedSchedules>>()
 				.inner
 				.get_mut(&label)
 				.unwrap()
-				.extend(children.to_vec());
+				.inner
+				.extend(container.inner);
 
 			self
 		}
